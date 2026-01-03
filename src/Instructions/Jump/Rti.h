e37c575d82501de116816ead293961f94d03aeea
NikitaVyaznikov <nvyiznik@gmail.com>
2026-01-03 20:05:15 +0300

    AdressMode was removed from Rti constructor

--- src/Instructions/Jump/Rti.h
+++ src/Instructions/Jump/Rti.h
@@ -7,7 +7,7 @@
 class Rti : public Jump, public HasFlags
 {
 public:
-    Rti(vector<uint8_t>::iterator begin, IAdressMode *adressMode, StackPointer *SP, Flags *fl) : Jump(begin, adressMode, SP), HasFlags(fl)
+    Rti(vector<uint8_t>::iterator begin, StackPointer *SP, Flags *fl) : Jump(begin, nullptr, SP), HasFlags(fl)
     {
     }
 
