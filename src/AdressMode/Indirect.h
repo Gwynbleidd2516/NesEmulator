5a85ab236c51fd34cea44bc60cba1b98ae08fefb
NikitaVyaznikov <nvyiznik@gmail.com>
2026-01-03 21:04:40 +0300

    Indirect mode is added

--- src/AdressMode/Indirect.h
+++ src/AdressMode/Indirect.h
@@ -2,26 +2,23 @@
 #define INDIRECT
 
 #include "IAdressMode.h"
-#include "Index.h"
 
-class IndirectX : public IAdressMode
+class Indirect : public IAdressMode
 {
 private:
     uint8_t *mMem;
     vector<uint8_t> *mPPU;
-    Index *mReg;
 
 public:
-    IndirectX(vector<uint8_t> &ppu, Index *reg)
+    Indirect(vector<uint8_t> &ppu)
     {
         mPPU = &ppu;
-        mReg = reg;
     }
 
     void code(vector<uint8_t>::iterator &it) override
     {
-        uint16_t buff = mPPU->at((int)*(it + 1) + (int)mReg->getValue());
-        mMem = &mPPU->at(buff);
+        it++;
+        mMem = &mPPU->at(mPPU->at(from8to16(*it, *(it + 1))));
         it++;
     }
 
@@ -35,47 +32,13 @@ public:
         return *mMem;
     }
 
-    // uint8_t *getResult() override
-    // {
-    //     return mMem;
-    // }
-};
-
-class IndirectY : public IAdressMode
-{
 private:
-    uint8_t *mMem;
-    vector<uint8_t> *mPPU;
-    Index *mReg;
-
-public:
-    IndirectY(vector<uint8_t> &ppu, Index *reg)
+    uint16_t from8to16(uint8_t a1, uint8_t a2)
     {
-        mPPU = &ppu;
-        mReg = reg;
+        uint16_t ans = a2;
+        ans *= 256;
+        return ans + a1;
     }
-
-    void code(vector<uint8_t>::iterator &it) override
-    {
-        uint16_t buff = mPPU->at((int)*(it + 1));
-        mMem = &mPPU->at(buff) + mReg->getValue();
-        it++;
-    }
-
-    void setValue(uint8_t val) override
-    {
-        *mMem = val;
-    }
-
-    uint8_t getValue() const override
-    {
-        return *mMem;
-    }
-
-    // uint8_t *getResult() override
-    // {
-    //     return mMem;
-    // }
 };
 
 #endif
---
+++ src/AdressMode/IndirectInd.h
@@ -0,0 +1,81 @@
+#ifndef INDIRECT_IND
+#define INDIRECT_IND
+
+#include "IAdressMode.h"
+#include "Index.h"
+
+class IndirectX : public IAdressMode
+{
+private:
+    uint8_t *mMem;
+    vector<uint8_t> *mPPU;
+    Index *mReg;
+
+public:
+    IndirectX(vector<uint8_t> &ppu, Index *reg)
+    {
+        mPPU = &ppu;
+        mReg = reg;
+    }
+
+    void code(vector<uint8_t>::iterator &it) override
+    {
+        uint16_t buff = mPPU->at((int)*(it + 1) + (int)mReg->getValue());
+        mMem = &mPPU->at(buff);
+        it++;
+    }
+
+    void setValue(uint8_t val) override
+    {
+        *mMem = val;
+    }
+
+    uint8_t getValue() const override
+    {
+        return *mMem;
+    }
+
+    // uint8_t *getResult() override
+    // {
+    //     return mMem;
+    // }
+};
+
+class IndirectY : public IAdressMode
+{
+private:
+    uint8_t *mMem;
+    vector<uint8_t> *mPPU;
+    Index *mReg;
+
+public:
+    IndirectY(vector<uint8_t> &ppu, Index *reg)
+    {
+        mPPU = &ppu;
+        mReg = reg;
+    }
+
+    void code(vector<uint8_t>::iterator &it) override
+    {
+        uint16_t buff = mPPU->at((int)*(it + 1));
+        mMem = &mPPU->at(buff) + mReg->getValue();
+        it++;
+    }
+
+    void setValue(uint8_t val) override
+    {
+        *mMem = val;
+    }
+
+    uint8_t getValue() const override
+    {
+        return *mMem;
+    }
+
+    // uint8_t *getResult() override
+    // {
+    //     return mMem;
+    // }
+};
+
+#endif
