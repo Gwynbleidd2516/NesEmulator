78d715cdc5d967277a6f72a0f33d9eea59572fcf
Nikita <nvyiznik@gmail.com>
2026-01-26 17:16:49 +0300

    Heirt is fixed

--- src/NesProgram/Registers/Index.h
+++ src/NesProgram/Registers/Index.h
@@ -5,19 +5,6 @@
 
 class Index : public Integer
 {
-private:
-    uint8_t mValue;
-
-public:
-    void setValue(uint8_t val) override
-    {
-        mValue = val;
-    }
-
-    uint8_t getValue() const override
-    {
-        return mValue;
-    }
 };
 
 #endif
