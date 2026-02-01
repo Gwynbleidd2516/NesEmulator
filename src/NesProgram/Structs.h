0621ea3c576067b03ba7dcd1dc2dd22a48412dae
Nikita <nvyiznik@gmail.com>
2026-02-01 17:10:21 +0300

    Header and Nesfile structs are removed

--- src/NesProgram/Structs.h
+++ src/NesProgram/Structs.h
@@ -7,30 +7,6 @@
 using namespace std;
 
 #pragma pack(push, 1)
-struct Header
-{
-    char format[4];
-    unsigned char rpgSize;
-    unsigned char chrSize;
-    unsigned char trainer;
-    unsigned char playChoice;
-    unsigned char rpgRamSize;
-    unsigned char tvSystem;
-    unsigned char tvRpgRam;
-    unsigned char padding[5];
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct NesFile
-{
-    Header head;
-    vector<uint8_t> code;
-    vector<uint8_t> data;
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
 struct CPU
 {
     struct RAM
