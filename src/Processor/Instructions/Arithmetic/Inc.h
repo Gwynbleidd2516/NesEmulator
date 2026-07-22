6c2d8f6cace5c740c46bd924c990e27aa87827bd
Nikita <nvyiznik@gmail.com>
2026-07-23 02:10:00 +0300

    Double inc is fixed

--- src/Processor/Instructions/Arithmetic/Inc.h
+++ src/Processor/Instructions/Arithmetic/Inc.h
@@ -12,18 +12,12 @@ class Inc : public HasAdressMode, public HasFlags
 public:
     Inc(IAdressMode *adressMode, Flags *fl) : HasAdressMode(adressMode), HasFlags(fl)
     {
-        // mReg = reg;
     }
-
-    // Inc(shared_ptr<IAdressMode> adressMode) : HasAdressMode(adressMode)
-    // {
-    //     mReg = reg;
-    // }
 
     void execute() override
     {
         uint8_t buf = mAdressMode->getValue() + 1;
-        mAdressMode->setValue(buf + 1);
+        mAdressMode->setValue(buf);
         mFlags->Zero = (buf == 0);
         mFlags->Negative = (buf >> 7);
         // (*mAdressMode->getResult())++;
