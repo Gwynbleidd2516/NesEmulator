3405f7b804e77c7567e9f5d1add59bfa8ceab39b
Nikita <nvyiznik@gmail.com>
2026-07-31 20:16:46 +0300

    Pattern loading is optimized

--- src/Render/Render.cpp
+++ src/Render/Render.cpp
@@ -28,6 +28,9 @@ void Render::loadPattern()
                     buf = (uint8_t)chr[j * 0x10 + 8 + k + l * 0x100].getBit(0x7 - i) << 1;
                     buf += (uint8_t)chr[j * 0x10 + k + l * 0x100].getBit(0x7 - i);
                     mTable1[i + j * 0x8 + k * 0x10 * 0x8 + l * 0x400] = buf * (float)(255.f / 3.f);
+                    buf = (uint8_t)chr[0x1000 + j * 0x10 + 8 + k + l * 0x100].getBit(0x7 - i) << 1;
+                    buf += (uint8_t)chr[0x1000 + j * 0x10 + k + l * 0x100].getBit(0x7 - i);
+                    mTable2[i + j * 0x8 + k * 0x10 * 0x8 + l * 0x400] = buf * (float)(255.f / 3.f);
                 }
             }
         }
@@ -42,23 +45,6 @@ void Render::loadPattern()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, mTextueBank1.getSize().x, mTextueBank1.getSize().y, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, &mTable1);
-
-    for (size_t l = 0; l < 0x10; l++)
-    {
-        for (size_t k = 0; k < 0x8; k++)
-        {
-            for (size_t j = 0; j < 0x10; j++)
-            {
-                for (int i = 0; i < 0x8; i++)
-                {
-                    uint8_t buf = 0;
-                    buf = (uint8_t)chr[0x1000 + j * 0x10 + 8 + k + l * 0x100].getBit(0x7 - i) << 1;
-                    buf += (uint8_t)chr[0x1000 + j * 0x10 + k + l * 0x100].getBit(0x7 - i);
-                    mTable2[i + j * 0x8 + k * 0x10 * 0x8 + l * 0x400] = buf * (float)(255.f / 3.f);
-                }
-            }
-        }
-    }
 
     glBindTexture(GL_TEXTURE_2D, mTextueBank2.getNativeHandle());
 
